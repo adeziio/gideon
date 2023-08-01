2e902ceaa3903f4b48428947cff7e5d295a48bcb
Aden <adeziio@yahoo.com>
2023-07-31 20:06:48 -0400

    update endpoint

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,14 +1,14 @@
 const domain = process.env.REACT_APP_ENV === "prod" ? "https://gideon-ai.vercel.app" : "http://127.0.0.1:5000";
 
 export const fetchChatBot = async (msg) => {
-    const res = await fetch(`${domain}/chat`, {
+    const res = await fetch(`${domain}/gpt-3.5-turbo`, {
         "method": "POST",
         "headers": {
             'Content-Type': 'application/json',
             "GIDEON_API_KEY": process.env.REACT_APP_GIDEON_API_KEY
         },
         "body": JSON.stringify({
-            "query": msg
+            "input": msg
         })
     })
     return await res.json()
